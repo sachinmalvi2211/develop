444222225ac18cc5ca6cf04159054affa3fc9122
sachin.malvi <sachinjagdish.malvi@hcl.com>
2023-07-12 06:41:34 +0530

    B2C storefront commit

---
+++ core-customize/hybris/bin/custom/demo/demotest/src/de/hybris/demo/test/constants/DemoTestConstants.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.demo.test.constants;
+
+/**
+ * 
+ */
+public class DemoTestConstants extends GeneratedDemoTestConstants
+{
+
+	public static final String EXTENSIONNAME = "demotest";
+
+}
